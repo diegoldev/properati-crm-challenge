7ab270dcabfa11c213562cfba00463f8b47a3bf1
Diego <diego.a.olmedo@gmail.com>
2021-06-13 14:52:24 -0300

    Add & configure Properties Model

---
+++ app/Models/PropertiesModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PropertiesModel extends Model
+{
+    public $timestamps = false;
+    protected $table = 'properties';
+    protected $casts = [
+        'property_type' => 'json',
+        'transaction_type' => 'json',
+        'city' => 'json',
+        'country' => 'json',
+        'state' => 'json',
+        'currency' => 'json',
+        'amenities' => 'array',
+        'payment' => 'array',
+        'disposition' => 'array',
+        'tags' => 'array',
+        'images' => 'array',
+        'google_map_data' => 'json',
+    ];
+
+}
